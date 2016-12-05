be994c535b7bcde0496aaf44e25a324257f534e8
Praktikum3 <prak3@i6.informatik.rwth-aachen.de>
2016-12-05 20:20:09 +0100

    backward skeleton

--- src/sietill/FeedForwardLayer.cpp
+++ src/sietill/FeedForwardLayer.cpp
@@ -28,6 +28,18 @@ namespace {
     }
     return FeedForwardLayer::Nonlinearity::None;
   }
+
+  double sigmoid(double x) {
+	  return 1 / (1 + exp(-x));
+  }
+
+  double tanh(double x) {
+	  return 2 * sigmoid(2*x) - 1;
+  }
+
+  double relu(double x) {
+	  return std::max(0.0, x);
+  }
 }
 
 const ParameterString FeedForwardLayer::paramNonlinearity("nonlinearity", "");
@@ -63,35 +75,68 @@ void FeedForwardLayer::backward_start() {
 void FeedForwardLayer::backward(std::valarray<float>& output, std::valarray<float>& error,
                                 std::gslice const& slice, std::vector<unsigned> const& mask) {
   // TODO: implement
-	// compute gradients
-  // Matrix of size D x H
+  unsigned max_time_step = *std::max_element(mask.begin(), mask.end());
+
+  // Matrix of size H x D
   std::gslice weights_slice(0, {output_size_, feature_size_}, {feature_size_, 1});
   std::valarray<float> weights = params_[weights_slice];
 
-  // dE_{total}/dw_{ij} = dE_{total}/dout_{ij} * dout{ij}/dnet_{ij} * dnet_{ij}/dw_{ij}
-  // 1: dE_{total}/dout_{ij} sum partial derivations w.r.t. the output
-  //std::valarray<float> sums_output_gradients = ??
+  // The bias is a matrix B x H. Each row of the matrix is the same!
+  std::valarray<float> bias(0.0, output_size_ * batch_size_);
+  for (size_t batch_idx = 0; batch_idx < batch_size_; batch_idx++) {
+    for (size_t output_idx = 0; output_idx < output_size_; output_idx++) {
+      bias[batch_idx * output_size_ + output_idx] = params_[output_size_ * feature_size_ + output_idx];
+    }
+  }
 
-  // 2: dout{ij}/dnet_{ij} how much does the output changes w.r.t. the input
-  //std::valarray<float> inner_gradients = ??
-	switch (nonlinearity_) {
-		case Nonlinearity::None:
-			break;
-		case Nonlinearity::Sigmoid:
-			break;
-		case Nonlinearity::Tanh:
-			break;
-		case Nonlinearity::ReLU:
-			break;
+  // Go over every time step
+	for (size_t time_idx = 0; time_idx < max_seq_length_; time_idx++) {
+	  if (time_idx > max_time_step) {
+	    break;
+	  }
+    // Slice of size B x H
+    std::gslice output_slice(time_idx * slice.size()[1] * slice.size()[2],
+                           {slice.size()[1], slice.size()[2]},
+                           {slice.stride()[1], slice.stride()[2]});
 
-		default:
-			break;
+    // dE_{total}/dw_{ij} = dE_{total}/dout_{ij} * dout{ij}/dnet_{ij} * dnet_{ij}/dw_{ij}
+    // 1: dE_{total}/dout_{ij} sum partial derivations w.r.t. the output
+    //std::valarray<float> sums_output_gradients = ??
+
+
+	  // 2: dout{ij}/dnet_{ij} how much does the output changes w.r.t. the input
+	  std::valarray<float> inner_gradients;
+		switch (nonlinearity_) {
+			case Nonlinearity::None:
+				inner_gradients = 1.0f;
+				break;
+			case Nonlinearity::Sigmoid:
+				//inner_gradients = sigmoid(0.0) * (1 - sigmoid(0.0));
+				inner_gradients = output[output_slice] * (1- output[output_slice]);
+				break;
+			case Nonlinearity::Tanh:
+				//inner_gradients = 1 - tanh(0.0) * tanh(0.0);
+				inner_gradients = 1 - (output[output_slice] * output[output_slice]);
+				break;
+			case Nonlinearity::ReLU:
+				float x = 0;
+				if (output[output_slice] > 0.0f) {
+					inner_gradients = 1.0f;
+				}
+				else {
+					inner_gradients = 0.0f;
+				}
+				break;
+			default:
+				break;
+		}
+
+
+	  // 3: dnet_{ij}/dw_{ij} how much does the input changes w.r.t. to the weights
+		//std::valarray<float> input_gradients = ??
+		// store gradients
+		//gradient_[slice] = sums_output_gradients * inner_gradients * input_gradients
 	}
-
-  // 3: dnet_{ij}/dw_{ij} how much does the input changes w.r.t. to the weights
-	//std::valarray<float> input_gradients = ??
-	// store gradients
-	//gradient_[slice] = sums_output_gradients * inner_gradients * input_gradients
 
 	// compute error if need
 	if (input_error_needed_) {
