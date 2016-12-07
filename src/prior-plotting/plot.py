5224357fdbe9789b3d78bceaacbfc15210502159
Konstantin <konerg@desktop.konerg.de>
2016-12-07 23:10:13 +0000

    different plots for prior

--- src/prior-plotting/plot.py
+++ src/prior-plotting/plot.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
-y = np.fromfile('prior.txt', sep=' ')
+y = np.fromfile('prior_alignment.txt', sep=' ')
+y2 = np.fromfile('prior_model1.txt', sep=' ')
+y3 = np.fromfile('prior_model5.txt', sep=' ')
 x = np.array(xrange(0, len(y)))
 
-plt.plot(x, y, label='State probabilities')
+plt.plot(x, y, label='alignment')
+plt.plot(x, y2, label='model_1')
+plt.plot(x, y3, label='model_5')
 #plt.xticks(x)
-
 plt.xlabel('State')
 plt.ylabel('Probability')
 plt.legend()
