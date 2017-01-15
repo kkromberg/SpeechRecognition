7ba90fbb65fe8cb7be33c4b4d705263917774111
Konstantin <konerg@desktop.konerg.de>
2017-01-15 01:05:56 +0100

    nGrams, count-counts

--- src/language-model/LanguageModel.py
+++ src/language-model/LanguageModel.py
@@ -18,9 +18,14 @@ class LanguageModel():
         self.allSentenceLength = {}
         self.totalSentenceLength = 0
         self.sortedWordFrequencies = 0
+        self.allTrigramOccurrence = self.computeNGramOccurrence(corpusFile, 3) # 2b
+        self.allTrigramFrequencies = self.countNGramsFrequencies(self.allTrigramOccurrence) # 2c
 
         self.initLM(corpusFile)
-        self.writeDictToFile(self.sortedWordFrequencies, 'wordFrequencies')
+        # write files
+        self.writeListToFile(self.sortedWordFrequencies, 'wordFrequencies')
+        self.writeListToFile(self.allTrigramOccurrence, 'trigramOccurrence')
+        self.writeDictToFile(self.allTrigramFrequencies, 'trigramFrequencies')
 
         self.averageSentenceLength = self.totalSentenceLength/self.numSentences
 
@@ -32,8 +37,11 @@ class LanguageModel():
         #print json.dumps(self.wordFrequencies, indent=2)
         #logging.debug('Word frequencies: ' + str(self.wordFrequencies))
 
-
     def initLM(self, corpusFile):
+        """
+        :param corpusFile: path to the corpus
+        :return:
+        """
 
         corpus = open(corpusFile, 'r')
         runningWords = set()
@@ -68,13 +76,84 @@ class LanguageModel():
         # 2a (sort)
         self.sortedWordFrequencies = sorted(wordFrequencies.items(), key=operator.itemgetter(1), reverse=True)
 
-    def writeDictToFile(self, data, outputFile):
+    def writeListToFile(self, data, outputFile):
+        """
+        Write data (list) into file (outputFile) separated by space
+        :param data:
+        :param outputFile:
+        :return:
+        """
         output = open(outputFile, 'w')
         for elem in data:
-            output.write(elem[0].replace('\n', '') + ' ' + str(elem[1]) + '\n')
+            output.write(str(elem[0]).replace('\n', '') + ' ' + str(elem[1]) + '\n')
         output.close()
+
+    def writeDictToFile(self, data, outputFile):
+        """
+        Write data (dict) into file (outputFile) separated by space
+        :param data:
+        :param outputFile:
+        :return:
+        """
+
+        output = open(outputFile, 'w')
+        for elem in data.iteritems():
+            output.write(str(elem[0]) + ' ' + str(elem[1]) + '\n')
+        output.close()
+
+    def computeNGramOccurrence(self, corpusFile, n):
+        """
+        Identify and compute n-grams from the given corpus file
+        :param corpusFile: path to the corpus
+        :param n: integer
+        :return:
+        """
+
+        corpus = open(corpusFile, 'r')
+        nGram = {}
+        for line in corpus:
+            currentSentences = line.replace(' ! ', ' . ').replace(' ? ', ' . ').split(' . ')
+            for sentence in currentSentences:
+                for punctuation in self.punctuations:
+                    if punctuation in sentence:
+                        sentence = sentence.replace(punctuation, '')
+                currentWords = filter(None, sentence.split(' ')) # remove empty entries caused by deleting punctuations
+                for i in range(0, len(currentWords)):
+                    currentNGram = ''
+                    if i < len(currentWords) -n:
+                        # add <s> for begin of the sentence
+                        if i == 0:
+                            currentNGram = '<s>'
+                        # add next n-1 elements
+                        for j in range(i, i+3):
+                            currentNGram += currentWords[j] + '|'
+
+                        # add </s> for end of the sentence
+                        if i == len(currentWords) - n - 1:
+                            currentNGram += '</s>'
+                    if currentNGram:
+                        if currentNGram not in nGram:
+                            nGram[currentNGram] = 1
+                        else:
+                            nGram[currentNGram] += 1
+
+        return sorted(nGram.items(), key=operator.itemgetter(1), reverse=True)
+
+    def countNGramsFrequencies(self, nGramOccurrence=dict()):
+
+        nGramFrequencies = {}
+        for elem in nGramOccurrence:
+            if elem[1] not in nGramFrequencies:
+                nGramFrequencies[elem[1]] = 1
+            else:
+                nGramFrequencies[elem[1]] += 1
+        #print nGramFrequencies
+        return nGramFrequencies
+
+
 
 corpusFile = '../../data/lm/corpus'
 lm = LanguageModel(corpusFile)
 
-plotRelativeSentenceLength(lm.allSentenceLength, lm.totalSentenceLength, lm.averageSentenceLength)
+#plotRelativeSentenceLength(lm.allSentenceLength, lm.totalSentenceLength, lm.averageSentenceLength)
+#plotDict(lm.allTrigramFrequencies)
--- src/language-model/Util.py
+++ src/language-model/Util.py
@@ -22,3 +22,23 @@ def plotRelativeSentenceLength(relativeSentenceLength, totalSentenceLength, aver
     plt.plot(x_range, Y, 'or')
     plt.axvline(x=averageSentenceLength-1)
     plt.show()
+
+
+def plotDict(data=dict()):
+
+    X = []
+    x_range = []
+    Y = []
+    counter = 0
+    for elem in data.iteritems():
+        X.append(elem[0])
+        Y.append(elem[1])
+        x_range.append(counter)
+        counter += 1
+    plt.title('Count-counts')
+    plt.xlabel('# nGrams')
+    plt.ylabel('Counts')
+    plt.xticks(x_range, X, rotation=90)
+    plt.plot(x_range, Y)
+    plt.plot(x_range, Y, 'or')
+    plt.show()
