a752921474619e5ec2743d60bde99c956bf8cf46
Konstantin <konerg@desktop.konerg.de>
2017-01-18 12:06:52 +0100

    remove print

--- src/language-model/Util.py
+++ src/language-model/Util.py
@@ -6,18 +6,15 @@ import numpy as np
 def plotAllSentenceLengths(sentenceLengths, averageSentenceLength):
     #print totalSentenceLength
     X = []
-    x_range = []
     Y = []
     counter = 0
     for elem in sentenceLengths.iteritems():
         X.append(elem[0])
         Y.append(math.log10(float(elem[1])))
-        x_range.append(counter)
         counter += 1
     plt.title('Occurrence of all sentence lengths in log space')
     plt.xlabel('Sentence length')
     plt.ylabel('log(sentenceLength)')
-    print x_range
     plt.xticks(np.arange(0, max(X), 5), rotation=90)
     plt.plot(X, Y)
     plt.plot(X, Y, 'or')
@@ -26,8 +23,6 @@ def plotAllSentenceLengths(sentenceLengths, averageSentenceLength):
     # saving manually results in better quality
     #plt.savefig('plots/allSentenceLengthsOccurrence.svg', format='svg', dpi=1200)
     plt.show()
-
-
 
 def plotDict(data=dict()):
 
