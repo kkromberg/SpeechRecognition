75a35900857102cebb5ebf62edd7405bf598bee3
Praktikum3 <prak3@i6.informatik.rwth-aachen.de>
2017-02-01 12:39:29 +0100

    added a python script to parse the XML output of the recognizer to calculate WER and RTF

---
+++ src/example-setup/statisticsFromLog.py
@@ -0,0 +1,32 @@
+import argparse
+import xml.etree.ElementTree as ET
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input", type=str, help ="Input XML file, which should be the log output of the sprint code.")
+args = parser.parse_args()
+
+elapsedTime = 0
+realTime = 0
+
+numberOfEdits = 0
+referenceLength = 0
+
+tree = ET.parse(args.input)
+root = tree.getroot()
+corpus = root.find('corpus')
+for recording in corpus.findall('recording'):
+	for segment in recording.findall('segment'):
+
+		# Get the values for the real time factor computation
+		elapsedTime += float(segment.find('timer').find('elapsed').text)
+		realTime += float(segment.find('real-time').text)
+
+		# Get the values for the WER computation
+		statistic = segment.find('evaluation').find('statistic')
+		numberOfEdits += float(statistic.find('cost').text)
+		referenceLength += float(statistic.find('count').text)
+
+
+
+print "Word Error Rate", numberOfEdits / referenceLength * 100
+print "Real Time Factor", elapsedTime / realTime 
